27726ec8537b3e28a6e50a81c7ff91d11fdb9101
SeanBecker <techtronixwebdesign@gmail.com>
2018-10-27 16:31:44 -0500

    updated - jumbotron and router links

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,6 +1,20 @@
 <template>
   <div class="home">
-    <h1>Home</h1>
+    <b-jumbotron>
+      <template slot="header">
+        Auth From Scratch
+      </template>
+      <template slot="lead">
+        An excellent guide to Authentication and Authorization 
+      </template>
+      <hr class="my-4">
+      <p>
+        Presented by Coding Garden
+      </p>
+      <b-btn variant="info">
+        <router-link :to="{ name: 'signup' }">Sign Up</router-link>
+      </b-btn>
+    </b-jumbotron>
   </div>
 </template>
 
