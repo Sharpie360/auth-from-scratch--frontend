eae2dbc4105a6427dd62ff1676d7bbdf14d26940
SeanBecker <techtronixwebdesign@gmail.com>
2018-10-28 23:39:41 -0500

    updated - CSS to RabbitWerks Theme 1.3

--- src/main.js
+++ src/main.js
@@ -1,7 +1,7 @@
 import Vue from 'vue';
 import BootstrapVue from 'bootstrap-vue';
-import 'bootstrap/dist/css/bootstrap.css';
 import 'bootstrap-vue/dist/bootstrap-vue.css';
+import './assets/bootstrapv3.css';
 
 import App from './App.vue';
 import router from './router';
