437d7aa0e8d85307300dd605a04994b6c59cdd38
SeanBecker <techtronixwebdesign@gmail.com>
2018-10-28 23:42:01 -0500

    updated - markup and styling, added rw logo, learn more btn router link

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,17 +1,19 @@
 <template>
   <div class="home">
-    <b-jumbotron>
+    <b-jumbotron class="pb-4">
+      <img src="../assets/rabbit-transparent-1000px.png" class="rw-logo">
       <template slot="header">
         Auth From Scratch
       </template>
       <template slot="lead">
-        An excellent guide to Authentication and Authorization 
+        An excellent guide to Authentication and Authorization for beginners!
       </template>
       <hr class="my-4">
       <p>
         Presented by Coding Garden
       </p>
-        <router-link class="btn btn-info" :to="{ name: 'signup' }">Sign Up</router-link>
+        <router-link class="btn btn-success mt-4 mr-3" :to="{ name: 'signup' }">Sign Up</router-link>
+        <router-link class="btn btn-outline-info mt-4" :to="{ name: 'signup' }">Learn More</router-link>
     </b-jumbotron>
   </div>
 </template>
@@ -22,3 +24,18 @@ export default {
   name: 'home',
 };
 </script>
+
+<style>
+
+.jumbotron {
+  position: relative;
+}
+
+.rw-logo {
+  width: 7rem;
+  position: absolute;
+  right: 1.5rem;
+  bottom: 1rem;
+}
+
+</style>
