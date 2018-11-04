9685a56deb6aa3675c0e4b1f407f1c853b442d37
Sean Becker <rabbitwerks.js@gmail.com>
2018-11-03 22:46:06 -0700

    updated - finished login form with loader, start of logic

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -1,10 +1,71 @@
 <template>
-  
+  <b-container>
+
+    <b-card>
+			<h1>{{ welcomeMessage }}</h1>
+			<p class="card-text">
+				please login to continue
+			</p>
+			<b-form
+				@sumbit.prevent="login">
+				<b-form-group
+					id="username-group"
+					label="Username:"
+					label-for="username-input"
+					description="please enter your username">
+					<b-form-input
+						id="username-input"
+						type="text"
+						v-model="user.username"
+						required>
+					</b-form-input>
+				</b-form-group>
+
+				<b-form-group
+					id="password-group"
+					label="Password:"
+					label-for="password-input"
+					description="please enter your password">
+					<b-form-input 
+						id="password-input"
+						type="password"
+						v-model="user.password"
+						required>
+					</b-form-input>	
+				</b-form-group>
+
+				<div class="submit-group">
+          <b-button type="submit" variant="success"><span v-show="!isRequesting">Login!</span>
+          <rotate-loader v-show="isRequesting"></rotate-loader>
+          </b-button>
+        </div>
+			</b-form>
+		</b-card>
+	</b-container>
 </template>
 
 <script>
-export default {
+import RotateLoader from '../components/RotateLoader'
 
+export default {
+	data () {
+		return {
+			welcomeMessage: 'Welcome!',
+			user: {
+				username: '',
+				password: ''
+			},
+			isRequesting: false
+		}
+	},
+	components: {
+		'rotate-loader': RotateLoader
+	},
+	methods: {
+		login() {
+
+		}
+	}
 }
 </script>
 
