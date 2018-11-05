2ad02f6af20d7d3fa652ccca69bf28c1f1e7cd2d
SeanBecker <techtronixwebdesign@gmail.com>
2018-11-04 23:26:01 -0600

    added - Dashboard.vue start

---
+++ src/views/Dashboard.vue
@@ -0,0 +1,24 @@
+<template>
+  <div>
+    <h1 class="display-1">Dashboard</h1>
+    <h6>it werks!</h6>
+  </div>
+</template>
+
+<script>
+
+export default {
+  data () {
+    return {
+      user: {
+        username: '',
+
+      }
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
