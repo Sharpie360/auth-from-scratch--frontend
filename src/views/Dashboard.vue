0e38c0ee0a8b23d8fd6ef0b7b1ad5e0218e754ef
SeanBecker <techtronixwebdesign@gmail.com>
2018-11-08 23:35:49 -0600

    updated - fetching user data from backend db on cmp mount

--- src/views/Dashboard.vue
+++ src/views/Dashboard.vue
@@ -1,19 +1,44 @@
 <template>
   <div>
     <h1 class="display-1">Dashboard</h1>
+    <h3>{{ user.username }}</h3>
     <h6>it werks!</h6>
+    <hr>
+    <b-button variant="danger" @click="logout">Logout</b-button>
   </div>
 </template>
 
 <script>
 
+const API_URL = 'http://localhost:7777'
+
 export default {
   data () {
     return {
       user: {
-        username: '',
-
+        
       }
+    }
+  },
+
+  mounted() {
+    fetch(API_URL, {
+      headers: {
+        authorization: `Bearer ${localStorage.token}`
+      }
+    }).then(res => res.json())
+    .then(result => {
+      if (result.user){
+        this.user = result.user
+      } else {
+        this.logout()
+      }
+    })
+  },
+  methods: {
+    logout() {
+      localStorage.removeItem('token')
+      this.$router.push('/login')
     }
   }
 }
