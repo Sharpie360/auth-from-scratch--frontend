56223b28de69fb2a7eaaabe98c59f25ef4166b07
SeanBecker <techtronixwebdesign@gmail.com>
2018-11-18 01:04:44 -0600

    added - Noteboard cmp

---
+++ src/components/Dashboard/Noteboard.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="grid-wrap mb-3">
+    <div class="note-wrapper" v-for="(note, i) in notes" :key="i">
+      <dash--note :note="note" :notesArrIndex="i"></dash--note>
+    </div>
+  </div>
+</template>
+
+<script>
+import Note from './Note'
+export default {
+  props: ['notes'],
+  components: {
+    'dash--note': Note
+  }
+}
+</script>
+
+<style>
+
+</style>
