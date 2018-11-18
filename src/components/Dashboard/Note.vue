b0e6bcc8d3a5aa5a988c0d87f7e5f90fa71e53b8
SeanBecker <techtronixwebdesign@gmail.com>
2018-11-18 01:05:20 -0600

    added - Note cmp with eventbus

---
+++ src/components/Dashboard/Note.vue
@@ -0,0 +1,48 @@
+<template>
+  <transition name="fade"
+    >
+    <b-card   
+      v-show="note"
+      class="note--card"
+      bg-variant="dark" 
+      border-variant="success"
+      :header="note.title">
+      <div 
+        class="delete-icon pointer" 
+        @click="Note_deleteNote(note._id, notesArrIndex)"
+        >X
+      </div>
+      <p 
+        class="card-text note--body"
+        >{{ note.body }}
+      </p>
+
+    </b-card>
+  </transition>
+</template>
+
+<script>
+import { EventBus } from '../../event-bus.js'
+
+export default {
+  props: ['note', 'notesArrIndex'],
+  methods: {
+    
+    Note_deleteNote(id, notesArrIndex) {
+      EventBus.$emit('deleteNote_trigger', {id, notesArrIndex})
+    }
+  }
+}
+</script>
+
+<style>
+
+.fade-enter-active, .fade-leave-active {
+  transition: opacity .5s ease-in-out;
+}
+.fade-enter, .fade-leave-after {
+  opacity: 0;
+}
+
+
+</style>
